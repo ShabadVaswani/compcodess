1113f07627190a3d6092dd4cf529d9a6b6e2465b
ShabadVaswani <72546893+ShabadVaswani@users.noreply.github.com>
2021-08-19 09:17:49 +0530

    Add files via upload

---
+++ cpptemplate.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+#define for0(i,x) for(int i = 0; i < x; i++)
+#define endl "\n"
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T;
+	cin>>T;
+	for0(i,T){
+        cout<<i<<endl;
+	
+	}
+}
